8000d413b928503b1c8bece53f69c8e730faa622
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-15 19:41:42 -0500

    Created the event handler that will recognize the numbers vs the operators and return the clicked item in the console

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let num2 = '';
 let operator = '';
 let total = '';
 
+$(document).ready(function() {
+    $('button').on('click', function(e) {
+        console.log('e', e.target.innerHTML);
+        let btn = e.target.innerHTML;
+        if (btn >= '0' && btn <= '9') {
+            console.log('number');
+        } else {
+            console.log('operator');
+        }
+    });
+});
