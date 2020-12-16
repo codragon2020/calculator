b9c637a4df0a8d4624516c02871306b03d918f10
Code JSON <73917308+codragon2020@users.noreply.github.com>
2020-12-15 20:07:57 -0500

    Added comments to functions

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let num2 = '';
 let operator = '';
 let total = '';
 
+
 $(document).ready(function() {
+    // To determine whether a button or operator was clicked, I can compare e.target.innerHTML to see if it is between 0 and 9. 
+    // If it is, the user clicked a number. If not, the user clicked an operator.
     $('button').on('click', function(e) {
         console.log('e', e.target.innerHTML);
         let btn = e.target.innerHTML;
@@ -15,6 +18,7 @@ $(document).ready(function() {
     });
 });
 
+// Function to determine if num1 is assigned, if it is num2 will be assigned 
 function handleNumber(num) {
     if (num1 === '') {
         num1 = num;
@@ -24,11 +28,26 @@ function handleNumber(num) {
     displayButton(num);
 }
 
+// Function to handle when an operator is pressed. Assign the value to our operator variable.
 function handleOperator(oper) {
     operator = oper;
     // displayButton(oper);
 }
 
+// Since we only update the display when the user presses a number
+// we can call the “displayButton” function from within the “handleNumber” function.
 function displayButton(btn) {
     $('.calc-result-input').text(btn);
+}
+
+// This function will create a total based on the operator pressed. 
+// Since there are multiple operators that can be pressed, we will use a case statement to handle them.
+function handleTotal() {
+    switch (operator) {
+        case '+':
+            // Converts the strings to numbers
+            total = +num1 + +num2;
+            displayButton(total);
+            break;
+    }
 }
